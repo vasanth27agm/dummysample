4febde471fdd494b68ca4e953a162d3a41efef84
vasanth27agm <vasanth27agm@gmail.com>
2025-10-29 23:10:51 +0530

    NEW PROJECT-1

---
+++ src/main/java/smallGitSample/Sample_1.java
@@ -0,0 +1,23 @@
+package smallGitSample;
+
+public class Sample_1 {
+	
+			private void Asuran() {
+			System.out.println("DHANUSH");
+			
+
+		}
+		
+		private void Arasan() {
+			System.out.println("SIMBU");
+
+		}
+		
+		private void Billa() {
+			System.out.println("AJITH");
+
+		}
+
+	
+
+}
